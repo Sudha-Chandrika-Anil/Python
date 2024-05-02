58a9e467a39f92060e9375a10549e62b67af67e5
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-05-03 00:25:04 +0530

    Create Math_Functions.py

---
+++ Math_Functions.py
@@ -0,0 +1,16 @@
+import math
+
+# Examples of math functions
+print("Square root of 16:", math.sqrt(16))
+print("2 raised to the power of 3:", math.pow(2, 3))
+print("Exponential of 2:", math.exp(2))
+print("Natural logarithm of 10:", math.log(10))
+print("Sine of π/2:", math.sin(math.pi / 2))
+print("Ceiling of 3.7:", math.ceil(3.7))
+print("Floor of 3.7:", math.floor(3.7))
+print("Absolute value of -5:", math.fabs(-5))
+print("Factorial of 5:", math.factorial(5))
+print("Integer square root of 16:", math.isqrt(16))
+print("GCD of 24 and 36:", math.gcd(24, 36))
+print("Value of π:", math.pi)
+print("Value of e:", math.e)
