80c2546692f62da4fa5729d3c9b3b18b1f6d8ea6
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-05-08 22:59:15 +0530

    Create Tuple_point_tdist.py

---
+++ Tuple_point_tdist.py
@@ -0,0 +1,10 @@
+import math
+
+# Tuple representing a 2D point (x, y)
+point = (3, 4)
+
+# Calculating the distance from the origin (0, 0)
+distance = math.sqrt(point[0] ** 2 + point[1] ** 2)
+
+# Printing the distance
+print("Distance from the origin:", distance)
